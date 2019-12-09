8f0ec704e211574f29048bdfedc4d84417703c23
zhang4417 <56223092+zhang4417@users.noreply.github.com>
2019-12-09 20:25:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -46,4 +46,4 @@ var server = http.createServer(function (request, response) {
 })
 
 server.listen(port)
-console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n请打开 http://localhost:' + port)
